60f396721975337e355bddfec3ac5339c2b31f5b
Ansarul Haq <mdansarul786940@gmail.com>
2024-02-01 23:00:38 +0545

    Add first c program

---
+++ Basic/program_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.141592
+int main()
+{
+	float radius,area,circumference;
+	printf("Enter radius of circle:");
+	scanf("%f",&radius);
+	area=PI * pow(radius,2);
+	circumference=2*PI*radius;
+	printf("Area of circle=%f\n",area);
+	printf("Circumference of circle=%f",circumference);
+return(0);
+}
