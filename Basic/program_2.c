f12ae7e0484a1e0179d9c4b02facd5335e74b22e
Ansarul Haq <mdansarul786940@gmail.com>
2024-02-02 22:23:37 +0545

    add program 2

---
+++ Basic/program_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int sum(int);
+int main()
+{
+	int n,res;
+	printf("Enter nth term:");
+	scanf("%d",&n);
+	res=sum(n);
+	printf("sum=%d",res);
+	return 0;
+}
+int sum(int num)
+{
+	if(num==1)
+	{
+		return(1);
+	}
+	else
+	{
+		return(sum(num-1)+num);
+	}
+}
