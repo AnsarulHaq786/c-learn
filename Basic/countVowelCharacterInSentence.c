23de8c0805834c2205bb0295d1153f08cab0b0c4
Ansarul Haq <mdansarul786940@gmail.com>
2024-03-09 21:22:42 +0545

    Add program documentation

--- Basic/countVowelCharacterInSentence.c
+++ Basic/countVowelCharacterInSentence.c
@@ -2,17 +2,17 @@
 #include<stdio.h>
 int main()
 {
-    int i,count=0;
+    int i,count=0; //intialize count to zero
     char in[50];
     printf("Enter sentence\n:");
-    gets(in);
+    gets(in); //input strings including blankspace
     for(i=0;i<50;i++)
     {
     if(in[i]=='a' || in[i]=='A' || in[i]=='e' || in[i]=='E' || in[i]=='i' || in[i]=='I' || in[i]=='o' || in[i]=='O' || in[i]=='u' || in[i]=='U')
-        {
+        { //condition for: if vowel letter is found
             count++;
         }
     }
     printf("Total no of vowel characters=%d",count);
-    return 0;
+    return 0; // end program without any error
 }
