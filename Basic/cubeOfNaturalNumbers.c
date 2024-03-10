6afd4075b79218a26c7131a5f1a22ae66eea2cf2
Ansarul Haq <mdansarul786940@gmail.com>
2024-03-10 21:58:55 +0545

    Add the programm documentation

--- Basic/cubeOfNaturalNumbers.c
+++ Basic/cubeOfNaturalNumbers.c
@@ -1,19 +1,19 @@
 // Sequence of Cube of numbers between given range
-#include<stdio.h>
-#include<math.h>
+#include<stdio.h> //uses->printf and scanf function
+#include<math.h> // for the use of pow function in line 14
 int main()
 {
     int n=0,a,b,i;
     printf("Enter range i,e a to b: \n");
-    scanf("%d%d",&a,&b);
+    scanf("%d%d",&a,&b); //input the lower and upper limit at the address of a and b
     printf("The cube of numbers within range are:\n ");
-    if(a<=b)
+    if(a<=b) // limit check to avoid infinity loop
     {
-        for (i=a;i<=b;i++)
+        for (i=a;i<=b;i++) // run the loop in between the range
         {
-            n=pow(i,3);
-            printf("%d\t",n);
+            n=pow(i,3); // assign cube of each digit
+            printf("%d\t",n); //display cubed output
         }
     }
-    return 0;
+    return 0; // terminate program with no error message
 }
