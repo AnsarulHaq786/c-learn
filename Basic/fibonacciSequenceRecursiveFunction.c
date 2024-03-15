3a16dcf491e9642d4ae2b666e8c88f478a5be2a0
Ansarul Haq <mdansarul786940@gmail.com>
2024-03-15 21:23:34 +0545

    Add documention to the c program

--- Basic/fibonacciSequenceRecursiveFunction.c
+++ Basic/fibonacciSequenceRecursiveFunction.c
@@ -1,21 +1,21 @@
 //fibonacci term using recursive function
 #include<stdio.h>
-int fibo(int);
+int fibo(int); //function declaration/prototyping
 int main()
 {
-	int n,result;
+	int n,result; //variable declaration int type
 	printf("Enter the position of term that should be printed(nth):");
 	scanf("%d",&n);
-	result=fibo(n);
-	printf("%d\t",result);
-	return 0;
+	result=fibo(n);// function calling and passing value stored in n and storing returned value into result variable
+	printf("%d\t",result); // display result in console
+	return 0; // end program with no error
 }
 int fibo(int n)
 {
-	if(n==0)
+	if(n==0) // compare if value of n is equal to 0 or not
 	return(0);
-	else if(n==1)
+	else if(n==1)// compare if value of n is equal to 1 or not
 	return(1);
 	else
-	return (fibo(n-1)+fibo(n-2));
+	return (fibo(n-1)+fibo(n-2)); // return sum of 2 previous values in function
 }
