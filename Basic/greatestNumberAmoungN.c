1932b3403f09e6a176bbe6d4f6ab37c54e3daa1c
Ansarul Haq <mdansarul786940@gmail.com>
2024-03-23 21:09:13 +0545

    Add documentation

--- Basic/greatestNumberAmoungN.c
+++ Basic/greatestNumberAmoungN.c
@@ -2,22 +2,22 @@
 #include<stdio.h>
 int main()
 {
-	int num[100],max,i,n;
+	int num[100],max,i,n; // num array with 100 maximum size for storing integer value
 	printf("Enter the quantity of numbers:");
-	scanf("%d",&n);
+	scanf("%d",&n);// count of different numbers
 	printf("Enter numbers:\n");
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&num[i]);
+		scanf("%d",&num[i]); // input n numbers in array from index 0 to n-1
 	}
-	max=num[0];
-	for(i=0;i<n;i++)
+	max=num[0]; // assgin value of first index of array as maximum
+	for(i=1;i<n;i++)
 	{
-		if(num[i]>max)
+		if(num[i]>max) // compare if other number in the upcomming index is greater than assigned value
 		{
-			max=num[i];
+			max=num[i];// if yes assign that value to max
 		}
 	}
-	printf("Largest number=%d",max);
+	printf("Largest number=%d",max);// display largest number in console
 	return 0;
 }
