4f96f929afb87fad459c4d984f667f750ff4a672
Ansarul Haq <mdansarul786940@gmail.com>
2024-03-26 21:54:55 +0545

    Add program documentation

--- Basic/palindromeNumberPromptAgain.c
+++ Basic/palindromeNumberPromptAgain.c
@@ -5,17 +5,17 @@ int main()
 	int ext,rev,num,org,i,n;
 	do
 	{
-		rev=0;
-		printf("Enter any number:");
-		scanf("%d",&num);
-		org=num;
-		while(num!=0)
-		{
-			ext=num%10;
-			rev=rev*10+ext;
-			num=num/10;
+		rev=0; //reset value of rev everytime when enter loop
+		printf("Enter any number:"); //prompt user any number
+		scanf("%d",&num); //input value at num variable
+		org=num; //assign org to num to secure identity
+		while(num!=0) //run loop unless num is not equal to 0
+		{// manipulation of num variable
+			ext=num%10; //extracts last digit from number
+			rev=rev*10+ext; //rewrite number from each decimal place in backward
+			num=num/10; //assign num variable removing last digit from number
 		}
-		if(org==rev)
+		if(org==rev) // check if entered number is equal to reversed number or not
 		{
 			printf("%d is Palindrome Number",org);
 		}
@@ -24,6 +24,7 @@ int main()
 			printf("%d is Not palindrome Number",org);
 		}
 		printf("\nWant to check for another number?\nPress\n'1' for Yes\n'0' for No:");
-		scanf("%d",&n);
-	}while(n!=0);
+		scanf("%d",&n); // read user choice
+	}while(n!=0); // if choice is no then loop ends
+	return 0;
 }
