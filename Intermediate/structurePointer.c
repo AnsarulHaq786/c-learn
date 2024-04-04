27982b59cfd18582ff76bd7be29b34cdf1022844
Ansarul Haq <mdansarul786940@gmail.com>
2024-04-04 21:23:13 +0545

    Add documentation

--- Intermediate/structurePointer.c
+++ Intermediate/structurePointer.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-struct student{
+struct student{ //stucture datatype defination
 	char name[20];
 	int roll;
 };
 int main()
 {
-	struct student s,*ptr;
-	ptr=&s;
-	printf("Enter name and roll of a student\n");
-	scanf("%s%d",s.name,&s.roll);
-	printf("Name\t|Roll\n");
-	printf("%s\t|%d",ptr->name,ptr->roll);
+	struct student s,*ptr; //declare variable s to struct student datatype
+	ptr=&s; // points to the initial address of s
+	printf("Enter name and roll of a student\n"); // prompt user to input name and roll
+	scanf("%s%d",s.name,&s.roll); // . selector is used to access elements inside structure variable
+	printf("Name\t|Roll\n"); // display result in tabular form
+	printf("%s\t|%d",ptr->name,ptr->roll); // -> selector to access element using pointer
 	return 0;
 }
