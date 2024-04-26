66b6382d244ff7cc82fc1b69021ef87b0680d297
Ansarul Haq <mdansarul786940@gmail.com>
2024-04-26 16:06:04 +0545

    Removed control function implementation possible in reset function and add transparency on the border

--- Projects/snakeStarGame.c
+++ Projects/snakeStarGame.c
@@ -1,16 +1,14 @@
-
 #include<stdio.h>
 #include<time.h>
 #include<unistd.h>
 #include<conio.h>
 #include<stdlib.h>
- int i,j,k,score=0,count=0,flag,foodX,foodY,rows=16,cols=30;
- int snakeX[10000],snakeY[10000],*sX=&snakeX[0],*sY=&snakeY[0];
+ int i,j,k,score=0,count=0,flag=0,foodX,foodY,rows=16,cols=30;
+ int snakeX[100000],snakeY[100000],*sX=&snakeX[0],*sY=&snakeY[0];
  char ch;
 void createBox();
 void createFood();
 void createSnake();
-void control(char);
 void reset(char);
 int checkCoordinate();
 void push();
@@ -19,7 +17,7 @@ int main()
 	createFood();
     createSnake();
     createBox();
-    control('l');
+    reset('l');
     return 0;
 }
 void createBox()
@@ -50,19 +48,18 @@ void createBox()
                 printf("|");
             else
             {
-            	int flag=0;
+            	int signal=0;
             	for(k=0;k<score;k++)
 				{
 					if(i==snakeX[count-k] && j==snakeY[count-k])
 					{
 						printf("o");
-						flag=1;
+						signal=1;
 					}
 				}
-				if(flag==0)
+				if(signal==0)
 				printf(" ");
-			}
-                
+			}     
         }
     }
     printf("\n\t  Score=%d",score);
@@ -80,102 +77,57 @@ void createSnake()
 	*sX=((rand()%(rows-2))+2);
     *sY=((rand()%(cols-2))+2);
 }
-void control(char ch)
-{
-    switch(ch)
-    {
-    	case 'j':
-    		reset(ch);
-    	break;
-    	case 'k':
-    		reset(ch);
-    	break;
-    	case 'i':
-    		reset(ch);
-    	break;
-    	case 'l':
-    		reset(ch);
-    	break;
-    	case 'q':
-    		exit(0);
-    	break;
-	}
-}
+
 void reset(char ch)
 {
-	while(ch=='j')
-	{
+    while(1)
+    {
         if(!kbhit())
         {
             system("cls");
             snakeX[count]=snakeX[0];
 			snakeY[count]=snakeY[0];
-            (*sY)--;
+            switch(ch)
+            {
+                case 'j':
+                    (*sY)--;
+                break;
+                case 'k':
+                    (*sX)++;
+                break;
+                case 'l':
+                    (*sY)++;
+                break;
+                case 'i':
+                    (*sX)--;
+                break;
+                case 'q':
+                exit(0);
+                break;
+                default :
+                break;
+            }
             flag=checkCoordinate();
             if(flag==1)
                 createFood();
             createBox();
             usleep(100000);
         }
-		else
-        control(getch());
-	}
-	while(ch=='i')
-	{
-        if(!kbhit())
-        {
-            system("cls");
-            snakeX[count]=snakeX[0];
-			snakeY[count]=snakeY[0];
-            (*sX)--;
-            flag=checkCoordinate();
-            if(flag==1)
-                createFood();
-            createBox();
-            usleep(100000);
-        }
-		else
-        control(getch());
-	}
-    while(ch=='k')
-	{
-        if(!kbhit())
-        {
-            system("cls");
-            snakeX[count]=snakeX[0];
-			snakeY[count]=snakeY[0];
-            (*sX)++;
-            flag=checkCoordinate();
-            if(flag==1)
-                createFood();
-            createBox();
-            usleep(100000);
-        }
-		else
-        control(getch());
-	}
-    while(ch=='l')
-	{
-        if(!kbhit())
-        {
-            system("cls");
-            snakeX[count]=snakeX[0];
-			snakeY[count]=snakeY[0];
-            (*sY)++;
-            flag=checkCoordinate();
-            if(flag==1)
-                createFood();
-            createBox();
-            usleep(100000);
-        }
-		else
-        control(getch());
-	}
+        else
+        reset(getch());
+    }
 }
 int checkCoordinate()
 {
-	if(*sX<=0 || *sY<=0 || *sX>=rows || *sY>=cols)
-	exit(0);
+	
+	if(*sY<=0)
+		*sY=cols;
+	else if(*sX<=0)
+		*sX=rows;
+	else if(*sX>=rows)
+		*sX=0;
+	else if(*sY>=cols)
+		*sY=0;
     if(foodX==*sX && foodY==*sY)
     {
     	score++;
